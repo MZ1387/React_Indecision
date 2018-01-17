fae0e44bf174faa6e04e1b9d9963542fad97d020
Mario Zamora <mariozamora@marios-mbp-2.attlocal.net>
2018-01-16 23:41:28 -0800

    Add Babel Compiler with env, react presets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,10 @@
-// var template = <p>JSX from app.js</p>;
-var template = React.createElement(
-    "p",
-    null,
-    "This is JSX"
-);
+'use strict';
 
+var template = React.createElement(
+  'p',
+  null,
+  'How can I try this out?'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = <p>How can I try this out?</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
