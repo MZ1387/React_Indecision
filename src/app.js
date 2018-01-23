83b6245deadcd84eacd31bb282260b232e442182
Mario Zamora <mariozamora@Marios-MBP-2.fios-router.home>
2018-01-22 22:18:58 -0800

    Save and Load State Data

--- src/app.js
+++ src/app.js
@@ -10,6 +10,32 @@ class IndecisionApp extends React.Component {
     }
   }
 
+  componentDidMount() {
+
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+
+      if (options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (e) {
+
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json)
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }))
   }
@@ -93,6 +119,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions} >Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option, index) => (
           <Option
@@ -135,6 +162,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }))
+
+    if (!error) {
+      event.target.elements.option.value = '';
+    }
   }
 
   render() {
