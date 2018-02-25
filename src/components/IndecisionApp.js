4cf3dbd7ec383569721138e1a96d0ee37b32f1d5
Mario Zamora <mariozamora@Marios-MBP-2.fios-router.home>
2018-02-24 21:33:20 -0800

    Create React Modal Component and Display Children

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,14 +3,20 @@ import AddOption from './AddOption';
 import Options from './Options';
 import Action from './Action';
 import Header from './Header';
+import OptionModal from './OptionModal';
 
 class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   };
   
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
+  };
+  
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
   };
 
   handleDeleteOption = (optionToRemove) => {
@@ -22,7 +28,7 @@ class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    console.log(option);
+    this.setState(() => ({ selectedOption: option }));
   };
 
   handleAddOption = (option) => {
@@ -78,6 +84,10 @@ class IndecisionApp extends React.Component {
         />
         <AddOption
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     );
